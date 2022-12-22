e589c438b58de8c3bb16166b15126083d6ffd2a2
TapToScreen <120685890+TapToScreen@users.noreply.github.com>
2022-12-22 23:30:06 +0500

    end

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from "./App"
+import { store } from './vuex/store'
+import router from './router/router'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
